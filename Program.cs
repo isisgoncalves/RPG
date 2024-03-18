09d66ab8de2cbe3839f0fd07599eec17e74d133a
isisgoncalves <isisvow@gmail.com>
2024-03-18 20:10:26 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        string nome = Console.ReadLine();
+
+        string raca = Console.ReadLine();
+
+        string classe = Console.ReadLine();
+
+        Personagem personagem = new Personagem(nome, raca, classe);
+
+        personagem.ExibirStatus();
+    }
+}
